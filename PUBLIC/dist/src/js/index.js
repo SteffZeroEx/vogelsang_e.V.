a8f1f8f0b625964877207ae82b50acbd31f49bd1
SteffZeroEx <163508274+SteffZeroEx@users.noreply.github.com>
2025-03-04 12:48:01 +0100

    Aktualisiere Titel der Seite, verbessere Login-Modal und implementiere Fehlerbehandlung für Benutzername und Passwort

--- PUBLIC/dist/src/js/index.js
+++ PUBLIC/dist/src/js/index.js
@@ -1,37 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //? FONTS LOADING
   document.fonts.ready.then(() => {
     document.documentElement.classList.add("wf-active");
   });
 
-  const logins = document.querySelectorAll(".login")
+  //? LOGIN AND OPEN MODAL
+  const logins = document.querySelectorAll(".login");
   for (const login of logins) {
-    login.addEventListener("click", openLogin); 
+    login.addEventListener("click", openLogin);
     login.addEventListener("touchstart", openLogin);
   }
-  
+
   const loginForm = document.querySelector(".loginForm");
   const closeLogin = document.querySelector(".login-close");
-  
+  const usernameInput = document.getElementById("usernameInput");
+  const passwordInput = document.getElementById("passwordInput");
+  const togglePassword = document.getElementById("togglePassword");
+  const loginBtn = document.getElementById("loginBtn");
+
   function openLogin(e) {
     console.log("Login-Modal öffnet sich");
     const loginSctn = document.querySelector(".login-sctn");
-  e.preventDefault();
+    e.preventDefault();
     loginSctn.classList.add("login-flex");
     loginSctn.classList.remove("login-none");
 
-    
-    closeLogin.addEventListener("click", closeLoginModal)
-      
-}
-function closeLoginModal(e) {
+    closeLogin.addEventListener("click", closeLoginModal);
+    loginSctn.addEventListener("click", (e) => {
+      if (!loginForm.contains(e.target)) {
+        closeLoginModal(e);
+      }
+    });
+
+    //? PASSWORD CHECK
+    loginBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const errorUser = document.querySelector(".error-user");
+      const errorPw = document.querySelector(".error-pw");
+
+      let isUsernameCorrect = false;
+      let isPasswordCorrect = false;
+
+      if (usernameInput.value === atob("Vm9yc3RhbmRWb2dlbHNhbmdTYXc=")) {
+        errorUser.classList.remove("error");
+        errorUser.innerText = "Der Name war richtig! (Ab hier erfolgen demnächst weitere Aktionen)";
+        isUsernameCorrect = true;
+      } else {
+        errorUser.innerText = "Der Benutzername ist ungültig";
+        errorUser.classList.add("error");
+      }
+
+      if (passwordInput.value === atob("Vm9yc3RhbmRTYXc")) {
+        errorPw.classList.remove("error");
+        errorPw.innerText = "Das Passwort war richtig! (Hier folgen demnächst weitere Aktionen)";
+        isPasswordCorrect = true;
+      } else {
+        errorPw.innerText = "Das Passwort ist ungültig!";
+        errorPw.classList.add("error");
+      }
+
+      if (isUsernameCorrect && isPasswordCorrect) {
+        alert(
+          "Ab hier erfolgen demnächst die Bereitstellungen von Buttons und weiteren Interaktionen zum Hinzufügen von Artikeln und zum Ändern der jeweiligen Nutzerdaten des Vereins"
+        );
+      }
+    });
+
+    //? SHOW LOGINPASSWORD
+    togglePassword.addEventListener("click", () => {
+      if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+      } else {
+        passwordInput.type = "password";
+      }
+    });
+  }
+  function closeLoginModal(e) {
     const loginSctn = document.querySelector(".login-sctn");
     e.preventDefault();
-      loginSctn.classList.remove("login-flex");
-      loginSctn.classList.add("login-none");
-  
-}
-  
+    loginSctn.classList.remove("login-flex");
+    loginSctn.classList.add("login-none");
+  }
 
+  //? NAVIGATION MENUE
   const menueBtn = document.querySelector(".menue-btn");
   menueBtn.addEventListener("click", (e) => {
     e.preventDefault();
